16a0138cb00419f6abc8c8406d3c797e313c95ac
Marcos Lascialanda <marcoslascialanda@gmail.com>
2021-04-19 12:19:45 -0300

    confim

---
+++ backend/data.js
@@ -0,0 +1,57 @@
+const data ={
+    products : [
+        {
+            _id:"1",
+            name:"Patineta",
+            category:"Long",
+            image:"https://http2.mlstatic.com/D_NQ_NP_2X_666578-MLA45297877732_032021-F.webp",
+            price:15000,
+            countInStock:10,
+            brand:"Long",
+            rating:4.5,
+            numReviews:10,
+            description:"El mejor LongBoard",
+        },
+        {
+            _id:"2",
+            category:"ShortBoard",
+            image:"https://http2.mlstatic.com/D_NQ_NP_2X_666578-MLA45297877732_032021-F.webp",
+            price:15000,
+            brand:"Long",
+            rating:2,
+            numReviews:10,
+            description:"El mejor LongBoard",
+        },
+        {
+            _id:"3",
+            category:"Skate",
+            image:"https://http2.mlstatic.com/D_NQ_NP_2X_666578-MLA45297877732_032021-F.webp",
+            price:15000,
+            brand:"Long",
+            rating:5,
+            numReviews:10,
+            description:"El mejor LongBoard",
+        },
+        {
+            _id:"4",
+            category:"Skate2",
+            image:"https://http2.mlstatic.com/D_NQ_NP_2X_666578-MLA45297877732_032021-F.webp",
+            price:15000,
+            brand:"Long",
+            rating:5,
+            numReviews:10,
+            description:"El mejor LongBoard",
+        },
+        {
+            _id:"5",
+            category:"Skate3",
+            image:"https://http2.mlstatic.com/D_NQ_NP_2X_666578-MLA45297877732_032021-F.webp",
+            price:15000,
+            brand:"Long",
+            rating:5,
+            numReviews:10,
+            description:"El mejor LongBoard",
+        },
+    ]
+};
+export default data;
---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import {BrowserRouter, Route} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            Amarzone
+          </a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/singin">Sing In</a>
+        </div>
+      </header>
+      
+      <main>
+      <Route path="/product/:id" component={ProductScreen}></Route>
+      <Route path="/" component={HomeScreen} exact ></Route>
+      </main>
+
+      <footer className="row center">All right reserved</footer>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
